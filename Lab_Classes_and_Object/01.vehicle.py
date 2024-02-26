dc27e3b29a6074df5a8fe40229c0b4937385a379
Angel Karasotyanov <maxaaa9@abv.bg>
2024-02-26 21:44:13 +0200

    Exercises

--- Lab_Classes_and_Object/01.vehicle.py
+++ Lab_Classes_and_Object/01.vehicle.py
@@ -1,15 +1,6 @@
-class Circle:
-    pi = 3.14
+class Vehicle:
 
-    def __init__(self, radius:int):
-        self.radius = radius
-
-    def set_radius(self, new_radius: int):
-        self.radius = new_radius
-
-    def get_area(self):
-        area = f"{Circle.pi * self.radius * self.radius:.2f}"
-        return float(area)
-
-    def get_circumference(self):
-        return 2 * Circle.pi * self.radius
+    def __init__(self, mileage: int, max_speed=150):
+        self.max_speed = max_speed
+        self.mileage = mileage
+        self.gadgets = []
