dfcc69c260aa5c6891f4282c09eca914416fb5f5
Angel Karasotyanov <maxaaa9@abv.bg>
2024-03-10 21:11:55 +0200

    Exercises

---
+++ Encapsulation_Lab/03_profile.py
@@ -0,0 +1,46 @@
+class Profile:
+
+    def __init__(self, username: str, password: str):
+        self.username = username
+        self.password = password
+
+    @property
+    def username(self):
+        return self.__username
+
+    @username.setter
+    def username(self, data):
+        if len(data) < 5 or len(data) > 15:
+            raise ValueError("The username must be between 5 and 15 characters.")
+        self.__username = data
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        valid_password = False
+        if len(value) >= 8:
+            is_upper = False
+            digit_in = False
+            for char in value:
+                if char.isupper():
+                    is_upper = True
+                elif char.isdigit():
+                    digit_in = True
+
+            if is_upper and digit_in:
+                valid_password = True
+
+        if not valid_password:
+            raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+
+        self.__password = value
+
+    def __str__(self):
+        return f'You have a profile with username: "{self.__username}" and password: {"*" * len(self.__password)}'
+
+
+
+profile_with_invalid_password = Profile('My_username', 'My-password')
