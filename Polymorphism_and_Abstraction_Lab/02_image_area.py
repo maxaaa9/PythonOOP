1965f64966dc6a41cb55b2fa3141f6d03c4ebc62
Angel Karasotyanov <maxaaa9@abv.bg>
2024-03-22 23:05:29 +0200

    Exercises

---
+++ Polymorphism_and_Abstraction_Lab/02_image_area.py
@@ -0,0 +1,20 @@
+class ImageArea:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.height * self.width
+
+    def __eq__(self, other):
+        return self.get_area() == other.get_area()
+
+    def __gt__(self, other):
+        return self.get_area() > other.get_area()
+
+    def __le__(self, other):
+        return self.get_area() <= other.get_area()
+
+    def __lt__(self, other):
+        return self.get_area() < other.get_area()
---
+++ Polymorphism_and_Abstraction_Lab/03_playing.py
@@ -0,0 +1,8 @@
+def start_playing(obj):
+    return obj.play()
+
+
+class Guitar():
+    @staticmethod
+    def play():
+        return "Playing the guitar"
